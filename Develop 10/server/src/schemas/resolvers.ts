c0171ff320ee4fda5a5fe1a6a33a7debf0abae99
Jake Walter <jakewalter080@gmail.com>
2025-01-04 14:14:08 -0600

    add export

--- Develop 10/server/src/schemas/resolvers.ts
+++ Develop 10/server/src/schemas/resolvers.ts
@@ -12,3 +12,18 @@ interface BookInput {
   link: string;
 }
 
+interface Context {
+    user?: UserDocument;
+  }
+
+  export const resolvers = {
+    Query: {
+      me: async (_: any, __: any, context: Context) => {
+        if (context.user) {
+          return await User.findById(context.user._id);
+        }
+        throw new AuthenticationError('Not logged in');
+      },
+    },
+
+    
