270423766a06236496a86721a94871299f6cee85
Jake Walter <jakewalter080@gmail.com>
2025-01-04 14:11:49 -0600

    add schemas to server

---
+++ Develop 10/server/src/schemas/typeDefs.ts
@@ -0,0 +1,12 @@
+import { gql } from 'apollo-server-express';
+
+export const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  
